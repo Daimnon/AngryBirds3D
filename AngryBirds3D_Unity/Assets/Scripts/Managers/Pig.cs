88fb86a07ba485467675806960b01e798d7b5899
Daimnon <49341329+Daimnon@users.noreply.github.com>
2024-12-02 19:55:36 +0200

    Last push

--- AngryBirds3D_Unity/Assets/Scripts/Managers/Pig.cs
+++ AngryBirds3D_Unity/Assets/Scripts/Managers/Pig.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class Pig : MonoBehaviour
 {
     [Header("Config")]
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private LayerMask _walkableLayer, _birdLayer;
+    [SerializeField] private float _runAwaySpeed = 4.0f;
     private Bird _detectedBird = null;
 
     [Header("Patrol")]
     [SerializeField] private float _distanceToNextPoint;
     [SerializeField] private Vector3 _nextPoint;
+    [SerializeField] private float _patrolSpeed = 3.0f;
     private bool _isNextPointSet = false;
 
     [Header("States")]
@@ -23,6 +25,7 @@ public class Enemy : MonoBehaviour
 
     private void Start()
     {
+        _agent.speed = _patrolSpeed;
         _state = PatrollingState;
     }
     private void OnEnable()
@@ -38,40 +41,81 @@ public class Enemy : MonoBehaviour
         _state.Invoke();
     }
 
+    #region States
     private void PatrollingState()
     {
-        if (_detectedBird)
+        if (!_isNextPointSet) LookForNextPoint();
+        if (_isNextPointSet) _agent.SetDestination(_nextPoint);
+
+        Vector3 distanceToTravel = transform.position - _nextPoint;
+        if (distanceToTravel.magnitude < 1.0f)
+        {
+            _isNextPointSet = false;
+        }
+
+        if (_detectedBird) // if bird exist (should be attached automatically)
         {
             float distanceFromBird = Vector3.Distance(_detectedBird.transform.position, transform.position);
             if (_detectBirdDistance < distanceFromBird)
             {
                 _isBirdDetected = true;
                 _state = InShockState; // state change
+                return;
             }
         }
     }
     private void InShockState()
     {
-        if (_detectedBird && _isBirdDetected)
+        if (_detectedBird && _isBirdDetected) // if bird exist (should be attached automatically), and detected
         {
             float distanceFromBird = Vector3.Distance(_detectedBird.transform.position, transform.position);
             if (_runAwayDistance < distanceFromBird)
             {
+                _agent.speed = _runAwaySpeed;
                 _state = RunAwayState; // state change
+                return;
             }
         }
     }
     private void RunAwayState()
     {
-        if (_detectedBird)
+        if (_detectedBird) // if bird exist (should be attached automatically)
         {
             float distanceFromBird = Vector3.Distance(_detectedBird.transform.position, transform.position);
-            if (distanceFromBird > _detectBirdDistance)
+            if (distanceFromBird > _runAwayDistance)
             {
+                _agent.speed = _patrolSpeed;
                 _state = PatrollingState; // state change
+                return;
             }
+
+            Vector3 birdDirection = (_detectedBird.transform.position - transform.position).normalized;
+            if (distanceFromBird <= _runAwayDistance)
+            {
+                Vector3 runAwayDestination = -birdDirection.normalized * _runAwaySpeed;
+                _agent.SetDestination(transform.position + runAwayDestination);
+            }
+            
+            return;
+        }
+
+        _agent.speed = _patrolSpeed;
+        _state = PatrollingState;
+    }
+    #endregion
+
+    private void LookForNextPoint()
+    {
+        float x = Random.Range(-_distanceToNextPoint, _distanceToNextPoint);
+        float z = Random.Range(-_distanceToNextPoint, _distanceToNextPoint);
+
+        _nextPoint = new(transform.position.x + x, transform.position.y, transform.position.z + z);
+        if (Physics.Raycast(_nextPoint, -transform.up, 2.0f, _walkableLayer))
+        {
+            _isNextPointSet = true;
         }
     }
+
     private void OnBirdSpawned(Bird bird)
     {
         _detectedBird = bird;
